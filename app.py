795baa20b3dc29c943b65e94fe23aef1347b0947
Jorge <r3b0rn.jhs@gmail.com>
2023-04-17 10:43:28 +0100

    add make responses for each api request

--- app.py
+++ app.py
@@ -1,41 +1,64 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
 from flask_restx import Api, Resource, fields
+
+from src.infrastructure.use_case_factory import *
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='Tmc2 Backend Code Challenge', description='API for managing your highlighted places')
 
+app.config['JSONIFY_MIMETYPE'] = 'application/json'
+
 resource_model = api.model('HighlightedPlace', {
-    'id': fields.Integer(required=True, description='ID'),
-    'name': fields.String(required=True, description='Name'),
-    'description': fields.String(required=True, description='Description'),
-    'coordinates': fields.String(required=True, description='Coordinates')
+    'name': fields.String(required=True, description='Name of your Highlighted Place'),
+    'description': fields.String(required=True, description='Description for your Highlighted Place'),
+    'coordinates': fields.String(required=True, description='Coordinates'),
 })
 
 
 @api.route('/api/highlight_place/<int:id>')
 @api.response(404, 'Highlighted Place not found')
 class HighlightedPlace(Resource):
-    @api.doc('get_resource')
+    @api.doc('get_highlight_place')
     def get(self, id):
-        pass
+        try:
+            result = get_highlighted_place_use_case().execute(highlighted_place_id=id)
+            return make_response(jsonify(result.to_dict()), 200)
+        except Exception as e:
+            print('ErrorAddingHighlightedPlace', e)
 
-    @api.doc('delete_resource')
+    @api.doc('delete_highlight_place')
     def delete(self, id):
-        pass
+        try:
+            result = delete_highlighted_place_use_case().execute(highlighted_place_id=id)
+            return make_response(jsonify(result.to_dict()), 200)
+        except Exception as e:
+            print('ErrorDeletingHighlightedPlace', e)
 
 
 @api.route('/api/highlight_place')
 class HighlightedList(Resource):
     @api.doc('list_highlight_places')
     def get(self):
-        pass
+        try:
+            result = get_all_highlighted_places_use_case().execute()
+            return make_response(jsonify([place.to_dict() for place in result]), 200)
+        except Exception as e:
+            print('ErrorListingHighlightedPlaces', e)
 
     @api.doc('create_highlight_place')
     @api.expect(resource_model)
     def post(self):
-        pass
+        try:
+            data = request.get_json()
+            name = data.get('name')
+            description = data.get('description')
+            coordinates = data.get('coordinates')
+            result = create_highlighted_place_use_case().execute(name, description, coordinates)
+            return make_response(jsonify(result.to_dict()), 200)
+        except Exception as e:
+            print('ErrorCreatingHighlightedPlace', e)
 
 
 PORT = int(os.environ.get('FLASK_PORT', 5000))
