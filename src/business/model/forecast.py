a4fd48249f17c1dd3a0edc0063e37d08a5a78d7c
Jorge <r3b0rn.jhs@gmail.com>
2023-04-17 10:43:58 +0100

    fix nonetype object has no attribute dict

--- src/business/model/forecast.py
+++ src/business/model/forecast.py
@@ -3,6 +3,12 @@ class Forecast:
         self.today = today
         self.tomorrow = tomorrow
 
+    def to_dict(self):
+        return {
+            'today': self.today.to_dict(),
+            'tomorrow': self.today.to_dict(),
+        }
+
 
 class ForecastDetails:
     def __init__(self, temperature, humidity, precipitation):
@@ -10,3 +16,9 @@ class ForecastDetails:
         self.humidity = humidity
         self.precipitation = precipitation
 
+    def to_dict(self):
+        return {
+            'temperature': self.temperature,
+            'humidity': self.humidity,
+            'precipitation': self.precipitation
+        }
